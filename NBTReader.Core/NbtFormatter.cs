ef2d4467ebd5055b696f0fa46bf07d95e1235687
crazygmr101 <dannash987@gmail.com>
2020-10-21 01:06:18 -0500

    Add basic reader compatibility

---
+++ NBTReader.Core/NbtFormatter.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.Text;
+using NBT;
+
+namespace NBTReader.Core
+{
+    public class NbtFormatter
+    {
+        public static string Format(TagCompound tag, int indent = 0)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            for (int i = 0; i < tag.Count; i++)
+            {
+                stringBuilder.AppendLine($"{new string(' ', indent)}{Format(tag[i], indent)}");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string Format(Tag tag, int indent = 0)
+        {
+            switch (tag.Type)
+            {
+                case TagType.Byte:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}b";
+                case TagType.Double:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}d";
+                case TagType.Int:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}i";
+                case TagType.Float:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}f";
+                case TagType.Long:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}L";
+                case TagType.Short:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}s";
+                case TagType.List:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.GetValue()}";
+                case TagType.Compound:
+                    TagCompound tagCompound = (TagCompound) tag;
+                    StringBuilder stringBuilder = new StringBuilder($"{new string(' ', indent)}{tag.Name.Trim()}:\n");
+                    for (int i = 0; i < tagCompound.Count; i++)
+                        stringBuilder.AppendLine($"{Format(tagCompound[i], indent + 1)}");
+                    return stringBuilder.ToString().TrimEnd();
+                // return $"{new string(' ', indent)}{tag.Name.Trim()}:" +
+                //       tagCompound.Flatten().Select(child => Format(child, indent + 4))
+                //           .Aggregate((working, end) => (working + "\n" + end));
+                case TagType.String:
+                    return $"{new string(' ', indent)}{tag.Name.Trim()}: \"{tag.GetValue()}\"";
+                case TagType.None:
+                    return "";
+                case TagType.IntArray:
+                    return
+                        $"{new string(' ', indent)}{tag.Name.Trim()}: {tag.Flatten().Select(child => child.GetValue()).Aggregate((working, next) => working + "," + next)}";
+            }
+
+            return tag.ToString();
+        }
+
+        static string FormatArray(int[] ar)
+        {
+            return ar.ToArray().Cast<string>().Aggregate((working, next) => working + "," + next);
+        }
+    }
+}
---
+++ NBTReader.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using NBT;
+using NBT.Serialization;
+
+
+namespace NBTReader.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(args[0], FileMode.Open);
+            TagReader tagReader = new BinaryTagReader(fileStream);
+            TagCompound root = tagReader.ReadDocument();
+            Console.Out.Write(NbtFormatter.Format(root));
+        }
+    }
+}
